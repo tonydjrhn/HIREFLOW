e5e9b4f3776ffb0565b965e9ed09fd411a44d082
AKSHAY KUMAR <akshaykumargmattur@gmail.com>
2026-08-12 19:40:35 +0530

    Serve frontend static files

--- server.js
+++ server.js
@@ -26,6 +26,7 @@ app.use(express.json());
 app.use(express.urlencoded({
     extended: true
 }));
+app.use(express.static(__dirname));
 // =====================================================
 // FRONTEND
 // =====================================================
