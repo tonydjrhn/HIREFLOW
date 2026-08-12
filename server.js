0094b25d646fc049f19a1965b37997201135fba8
AKSHAY KUMAR <akshaykumargmattur@gmail.com>
2026-08-12 18:12:29 +0530

    Serve frontend from Express

--- server.js
+++ server.js
@@ -26,6 +26,15 @@ app.use(express.json());
 app.use(express.urlencoded({
     extended: true
 }));
+// =====================================================
+// FRONTEND
+// =====================================================
+
+app.use(express.static(__dirname));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
 
 
 // =====================================================
